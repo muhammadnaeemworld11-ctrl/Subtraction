ddf0f6f71ff4343df05881f071714c4635ba38f1
muhammadnaeemworld11-ctrl <muhammadnaeemworld11@gmail.com>
2025-12-15 07:36:28 -0800

    Add files via upload

---
+++ Subtraction.py
@@ -0,0 +1,5 @@
+print("SIMPLE SUBTRACTION CALCULATOR ")
+
+num1 = int(input("Enter first number to subtrac: "))
+num2 = int(input("Enter second number to subtrac: "))
+print(f"{num1} - {num2} = {num1-num2}")
